7cb411255b0c72032b741627289e47759081ee42
kabeersheikh <kabeersheikh008@gmail.com>
2025-01-09 11:28:33 +0500

    Your  here

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
       {/* Tasks Section */}
       <ul className="space-y-3">
-        <li className="flex items-center justify-between bg-gray-100 rounded-lg px-4 py-2 shadow">
+        <li className="flex items-center justify-between bg-gray-99 rounded-lg px-4 py-2 shadow">
           <span className="text-gray-800">Learn React</span>
           <button className="text-red-500 hover:text-red-700 transition">
             ✖
